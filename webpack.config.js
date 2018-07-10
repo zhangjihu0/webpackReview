3155c777249e844e5b732132364abf8edf5e3b9f
v.jihu.zhang <v.jihu.zhang@deltaww.com>
2018-07-10 18:18:42 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 //webpack是基于node的所以要遵循commonjs的规范
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports={
   entry:'',//入口
   output:{
-    filename:'build.js',
+    filename:'build.[hash:8].js',//每次打包后都会有新的hash.生成
     //绝对路径
     path:path.resolve('./build')
   },//出口
@@ -14,7 +16,18 @@ module.exports={
     hot:true,
   },//开发服务器
   module:{},//模块配置
-  plugins:[],//插件的配置
+  plugins:[
+    new CleanWebpackPlugin(['./build']),
+    new HtmlWebpackPlugin({
+      template:'./src/index.html',
+      title:'啦啦啦',
+      hash:true,
+      // minify:{
+      //   removeAttributeQuotes:true,
+      //   collapseWhitespace:true
+      // }
+    })
+],//插件的配置
   mode:'development',//可以更改模式
   resolve:{},//配置解析
 }
